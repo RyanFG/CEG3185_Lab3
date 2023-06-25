52d90ab35f7dc402f21aa8fc0b9f08438aadc47f
Lys02 <137233021+Lys02@users.noreply.github.com>
2023-06-25 17:34:16 -0400

    Update PacketReceiver.java

--- PacketReceiver.java
+++ PacketReceiver.java
@@ -113,3 +113,132 @@ public class PacketReceiver{
         PacketReceiver receiver = new PacketReceiver(args[0],Integer.parseInt(args[1]));
     }
 }
+
+
+/*import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class PacketReceiver{ 
+
+	//initialize socket and input stream 
+	private Socket socket;
+	private ServerSocket MyServer;
+	private DataInputStream in;
+
+	
+
+	public PacketReceiver(int port){ 
+		try{ 
+		
+			MyServer = new ServerSocket(port); 
+			//System.out.println("Server started"); 
+
+			//System.out.println("Waiting for a client ..."); 
+
+	
+			socket = MyServer.accept(); 
+			System.out.println("Client accepted"); 
+
+			
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream())); 
+
+			String line = ""; 
+
+			while (!line.equals("Stop")) 
+			{ 
+				try
+				{ 
+					line = in.readUTF(); 
+					System.out.println(line); 
+
+				} 
+				catch(IOException i) 
+				{ 
+					System.out.println(i); 
+				} 
+			} 
+			System.out.println("Closing connection"); 
+
+	
+			socket.close(); 
+			in.close(); 
+		} 
+		catch(IOException e) 
+		{ 
+			System.out.println(e); 
+		} 
+	} 
+	public void DatagramReceiver( byte [] buf , int port){
+		try{
+			DatagramSocket dsck = new DatagramSocket(port);
+			buf = new byte[20];
+			DatagramPacket packet = new DatagramPacket(buf, port);
+			dsck.receive(packet);
+			String str = new String(packet.getData(), 0, packet.getLength());
+			System.out.println(str);
+			dsck.close();
+			}
+			catch(SocketException e) 
+			{ 
+				System.out.println(e); 
+			}
+			catch (UnknownHostException e) {
+			System.out.println(e);
+			}
+			catch(IOException e)
+			{
+				System.out.println(e);
+				} 
+	}
+	
+	 public String decoder(String msg){
+        String text = toString(msg);
+        return text;
+    }
+
+    public String toString(String str){
+        String result = new String();
+        char[] charArray = str.toCharArray();
+        for(int i = 0; i < charArray.length; i=i+2) {
+            String st = ""+charArray[i]+""+charArray[i+1];
+            char ch = (char)Integer.parseInt(st, 16);
+            result = result + ch;
+        }
+        return result;
+
+    }
+	public String checkSumReceiver(String s) {
+        String hexString = new String();
+        int x, i, checkSum = 0;
+        decoder(hexString);
+        x = integer.toString(hexString)
+
+        checkSum += x;
+
+        if (hexString.length() > 8) {
+            int carry = Integer.parseInt(("" + hexString.charAt(0)), 16);
+
+            checkSum = Integer.parseInt(hexadecimalValue, 16);
+
+            checkSum += carry;
+        }
+        
+        checkSum = checkComplement(checkSum);
+
+        
+        return (Integer.toString(checkSum)); 
+    }
+	int checkComplement(int checkSum) {
+
+        checkSum = Integer.parseInt("FFFF", 16) - checkSum;
+        return checkSum;
+    }
+    public static void main(String args[]) { 
+		 
+	} 
+}
+*/
+	
+	
+	
