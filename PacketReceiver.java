6b342b13952da0f77bc7043c2da1527a58a90fa9
Ryan Frost-Garant <rfros048@uottawa.ca>
2023-06-26 17:20:34 -0400

    connection not working not sure why

--- PacketReceiver.java
+++ PacketReceiver.java
@@ -8,7 +8,7 @@ public class PacketReceiver{
     private DataInputStream input = null;
 
 
-    public PacketReceiver(String address, int portNum){
+    public PacketReceiver(int portNum){
 
         try{
             MyServer = new ServerSocket(portNum);
@@ -25,8 +25,8 @@ public class PacketReceiver{
                 try{
                     line = input.readUTF();
 
-                    decoded = decoder(line.substring(54));
-                    if(decoder(line.substring(54)).equals("over")){
+                    decoded = decoder(line);
+                    if(decoder(line.equals("Over"))){
                         break;
                     }
 
@@ -60,62 +60,14 @@ public class PacketReceiver{
         return result;
     }
 
-     /*
-    public void DatagramIP(byte [] buffer, int length){
-        DatagramSocket s = new DatagramSocket ();
-        byte [] data = new byte [0000];
-        DatagramPacket dgrm = new DatagramPacket (data, data.length);
-        while (true){
-            s.receive (dgrm);
-            System.out.println (new String (data));
-            s.send (dgrm);
-        }
-    }
-
-    public void ChecksumReceiver(InetAddress ip,int port)throws IOException {
-        c = new Socket(ip,port);
-        input = new DataInputStream(socket.getInputStream());
-        output = new DataOutputStream(socket.getOutputStream());
-
-        while (true){
-            int i, l, sum = 0, numb;
-            l = data.length;
-            int data[] = new string[l];
-            int c_data[] = new string [l];
-            System.out.println("Data received (along with checksum) is");
-            
-            for (i = 0; i < data.length; i++){
-                data[i] = input.nextInt();
-                System.out.println(data[i]);
-                
-                numb = (int)(Math.floor(Math.log(data[i]) / Math.log(2))) + 1;				 
-                c_data[i] = ((1 << numb) - 1) ^ data[i];
-                
-                System.out.println("Calculated Checksum is : "+sum);
-                
-                if(sum == 0){
-                    output.writeUTF("success");
-                    break;
-                }
-                else{
-                    output.writeUTF("failure");
-                    break;
-                }
-                input.close();
-                output.close();
-                c.close();	
-            }		
-        }			 
-    }
-    */
 
     public static void main(String[] args){
-        PacketReceiver receiver = new PacketReceiver(args[0],Integer.parseInt(args[1]));
+        PacketReceiver receiver = new PacketReceiver(Integer.parseInt(args[0]));
     }
 }
+/*
 
-
-/*import java.io.*;
+import java.io.*;
 import java.net.*;
 import java.util.*;
 
@@ -133,9 +85,7 @@ public class PacketReceiver{
 		
 			MyServer = new ServerSocket(port); 
 			//System.out.println("Server started"); 
-
 			//System.out.println("Waiting for a client ..."); 
-
 	
 			socket = MyServer.accept(); 
 			System.out.println("Client accepted"); 
@@ -169,30 +119,8 @@ public class PacketReceiver{
 			System.out.println(e); 
 		} 
 	} 
-	public void DatagramReceiver( byte [] buf , int port){
-		try{
-			DatagramSocket dsck = new DatagramSocket(port);
-			buf = new byte[20];
-			DatagramPacket packet = new DatagramPacket(buf, port);
-			dsck.receive(packet);
-			String str = new String(packet.getData(), 0, packet.getLength());
-			System.out.println(str);
-			dsck.close();
-			}
-			catch(SocketException e) 
-			{ 
-				System.out.println(e); 
-			}
-			catch (UnknownHostException e) {
-			System.out.println(e);
-			}
-			catch(IOException e)
-			{
-				System.out.println(e);
-				} 
-	}
-	
-	 public String decoder(String msg){
+
+	public String decoder(String msg){
         String text = toString(msg);
         return text;
     }
@@ -212,14 +140,12 @@ public class PacketReceiver{
         String hexString = new String();
         int x, i, checkSum = 0;
         decoder(hexString);
-        x = integer.toString(hexString)
+        x = Integer.parseInt(toString(hexString));
 
         checkSum += x;
 
         if (hexString.length() > 8) {
             int carry = Integer.parseInt(("" + hexString.charAt(0)), 16);
-
-            checkSum = Integer.parseInt(hexadecimalValue, 16);
 
             checkSum += carry;
         }
@@ -229,7 +155,7 @@ public class PacketReceiver{
         
         return (Integer.toString(checkSum)); 
     }
-	int checkComplement(int checkSum) {
+	public int checkComplement(int checkSum) {
 
         checkSum = Integer.parseInt("FFFF", 16) - checkSum;
         return checkSum;
--- PacketSender.java
+++ PacketSender.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class PacketSender{
 
@@ -27,14 +28,15 @@ public class PacketSender{
         String line = "";
         String encoded = "";
 
-        while (!line.equals("over")){
+        while (!line.equals("Over")){
             try{
                 line = input.readLine();
 
-                encoded = encoder(line);
+                encoded = encoder(address,line);
 
                 output.writeUTF(encoded);
             }
+
             catch (IOException i) {
                 System.out.println(i);
             }
@@ -44,28 +46,64 @@ public class PacketSender{
             output.close();
             MyClient.close();
         }
+
         catch (IOException i) {
             System.out.println(i);
         }
     }
 
-    public String encoder(String msg){
+    public String encoder(String address,String msg){
         String all = "";
         String hexa = toHexa(msg);
+
         String datagram = "";
         String header = "4500";
-        Double num = 20.0+(hexa.length()/2);
-        String headerIP = toHexa(Integer.toString((int)Math.floor(num)));
+        int num = (int)Math.floor(20+(hexa.length()/2));
+        String headerIP = Integer.toHexString(num);
+        while(headerIP.length() < 4){headerIP = "0"+headerIP;}
         String ident = "1C46";
         String flag = "4000";
         String ttl = "4006";
         String checksum = "0000";
-        String srcIP = toHexa("192.168.0.3");
-        String dstIP = toHexa("192.168.0.1");
 
-        String sum = hexaAdd(header,headerIP,ident,flag,ttl,checksum,srcIP,dstIP);
+        //Converting source address to ints and then ints to 2-bit hexadecimal values
+        //first 4-bits of source (Ex. 192 -> C0, 168 -> A3 = C0A3)
+        String src1 = Integer.toHexString(Integer.parseInt(address.substring(0,address.indexOf("."))));
+        if(src1.length() < 2){src1 = "0"+src1;}
+        String address1 = address.replace(String.valueOf(src1)+".","");
+        String src2 = Integer.toHexString(Integer.parseInt(address1.substring(0,address1.indexOf("."))));
+        if(src2.length() < 2){src2 = "0"+src2;}
+        String srcHex1 = src1+src2;
 
-        datagram = header+headerIP+ident+flag+ttl+checksum+srcIP+dstIP;
+        //second 4-bits of source (Ex. 0 -> 00, 3 -> 03 = 0003)
+        String address2 = address1.replace(String.valueOf(src2)+".","");
+        String src3 = Integer.toHexString(Integer.parseInt(address2.substring(0,address2.indexOf("."))));
+        if(src3.length() < 2){src3 = "0"+src3;}
+        String address3 = address2.replace(String.valueOf(src3)+".","");
+        String src4 = Integer.toHexString(Integer.parseInt(address3.substring(0,address3.indexOf("."))));
+        if(src4.length() < 2){src4 = "0"+src4;}
+        String srcHex2 = src3+src4;
+
+        //create destination IP address
+        Random random = new Random();
+        String dst1 = Integer.toHexString((random.nextInt(256)));
+        if(dst1.length() < 2){dst1 = "0"+dst1;}
+        String dst2 = Integer.toHexString((random.nextInt(256)));
+        if(dst2.length() < 2){dst2 = "0"+dst2;}
+        String dstHex1 = dst1+dst2;
+
+        String dst3 = Integer.toHexString((random.nextInt(256)));
+        if(dst3.length() < 2){dst3 = "0"+dst3;}
+        String dst4 = Integer.toHexString((random.nextInt(256)));
+        if(dst4.length() < 2){dst4 = "0"+dst4;}
+        String dstHex2 = dst3+dst4;
+
+
+        String sum = hexaAdd(Integer.parseInt(header,16),Integer.parseInt(headerIP,16),Integer.parseInt(ident,16),Integer.parseInt(flag,16),
+        Integer.parseInt(ttl,16),Integer.parseInt(checksum,16),Integer.parseInt(srcHex1,16),Integer.parseInt(srcHex2,16),
+        Integer.parseInt(dstHex1,16),Integer.parseInt(dstHex2,16));
+
+        datagram = header+headerIP+ident+flag+ttl+checksum+srcHex1+srcHex2+dstHex1+dstHex2;
         all = datagram+hexa;
 
         return all;
@@ -81,90 +119,20 @@ public class PacketSender{
         }
 
         String result = sb.toString();
+        System.out.println(result);
         return result;
     }
 
-    public String hexaAdd(String header,String headerIP,String ident,String flag,String ttl,String checksum,String srcIP,String dstIP){
-        String src1 = srcIP.substring(0,3);
-        String src2 = srcIP.substring(4,7);
-        String dst1 = dstIP.substring(0,3);
-        String dst2 = dstIP.substring(4,7);
+    public String hexaAdd(int header,int headerIP,int ident,int flag,int ttl,int checksum,int srcIP1,int srcIP2, int dstIP1, int dstIP2){
+        int sum = header+headerIP+ident+ttl+checksum+srcIP1+srcIP2+dstIP1+dstIP2;
 
-        String sum = "";
-        return sum;
-    }
-
-    /*
-    public void DatagramIP(byte [] buffer, int length){
-        DatagramSocket s = null;
-        try{
-            s = new DatagramSocket();
-            byte buffer;
-            buffer = new String ("Send me a datagram").getBytes ();
-            InetAddress ip = InetAddress.get("192.168.0.3");
-            
-            dgrm = new DatagramIP ();
-            
-            s.send(dgrm);
-            
-            
-            byte [] buffer2 = new byte [0000];
-            dgrm = new DatagramIP (buffer2, buffer.length, ip, portNum);
-            s.receive (dgrm);
-            
-            System.out.println (new String (dgrm.getData ()));
-		}
-		catch (IOException e){
-			System.out.println (e.toString ());
-		}
-		finally{
-			if (s != null)
-			    s.close ();
-		}
-	}
-
-    public void ChecksumSender(InetAddress ip,int port)throws IOException {
-        c = new Socket(ip,port);
-        input = new DataInputStream(socket.getInputStream());
-        output = new DataOutputStream(socket.getOutputStream());
-        while (true){
-        int i, l, sum = 0, numb;
-        l = data.length;
-        int data[] = new string [MAX];
-		int c_data[] = new string [MAX];
-		System.out.println("data to send");
-		 
-		 
-		for (i = 0; i < l; i++){
-			data[i] = sc.nextInt();
-			numb = (int)(Math.floor(Math.log(data[i]) / Math.log(2))) + 1;
-			 
-			c_data[i] = ((1 << numb) - 1) ^ data[i];
-			 
-			sum += c_data[i];
-		}
-		 
-		data[i] = sum;
-        l += 1;
-         
-        System.out.println("Checksum Calculated is : " + sum);
-        System.out.println("Data being sent along with Checksum.....");
-		
-		if (dis.readUTF().equals("failure")){
-			System.out.println("Message was not received successfully!");
-			break;
-		}
-		else if (dis.readUTF().equals("success")){
-			System.out.println("Message received Successfully!");
-			break;			
+        String hexsum = Integer.toHexString(sum);
+        while(sum > 65535){
+            int s = sum-65535;
         }
-	}
-	output.close();
-	input.close();
-	socket.close();
-	  
+
+        return hexsum;
     }
-    */
 
     public static void main(String[] args){
         PacketSender sender = new PacketSender(args[0],Integer.parseInt(args[1]));
@@ -186,75 +154,40 @@ public class PacketSender {
 	
 	public PacketSender(InetAddress address, int port){ 
 
-		try
-		{ 
-			
+		try{ 			
 			MyClient = new Socket(address, port); 
 			System.out.println("Connected"); 
 
-			
 			input = new BufferedReader(new InputStreamReader(System.in));
-
-			
+		
 			output = new DataOutputStream(MyClient.getOutputStream()); 
 		} 
-		catch(UnknownHostException e) 
-		{ 
+		catch(UnknownHostException e) { 
 			System.out.println(e); 
 		} 
-		catch(IOException e) 
-		{ 
+		catch(IOException e) { 
 			System.out.println(e); 
 		} 
-
 		
 		String line = ""; 
 
-		
-		while (!line.equals("Stop")) 
-		{ 
-			try
-			{ 
+		while (!line.equals("Stop")) { 
+			try{ 
 				line = input.readLine();
 				
 				output.writeUTF(line); 
 			} 
-			catch(IOException i) 
-			{ 
+			catch(IOException i) { 
 				System.out.println(i); 
 			} 
 		} 
-		try
-		{ 
+		try{ 
 			input.close(); 
 			output.close(); 
 			MyClient.close(); 
 		} 
-		catch(IOException i) 
-		{ 
+		catch(IOException i) { 
 			System.out.println(i); 
-		} 
-	} 
-	
-	public void DatagramSender(byte [] buf, int length, InetAddress address, int port){
-		try{
-		 DatagramSocket dsck = new DatagramSocket ();
-		 String msg = new String();
-		 address = InetAddress.getLocalHost();
-		 DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), address, port);
-		 dsck.send(packet);
-		 dsck.close();
-		}
-		catch(SocketException e) 
-			{ 
-				System.out.println(e); 
-			}
-		catch (UnknownHostException e) {
-			System.out.println(e); 
-		}
-		catch(IOException e) 
-		{ 
-			System.out.println(e); 
 		} 
 	}
 		
@@ -288,5 +221,10 @@ public class PacketSender {
             System.out.println("The verification of the checksum demonstrates that the packet received is correct.");
         }
     }
+
+        public static void main(String[] args){
+        PacketSender sender = new PacketSender(args[0],Integer.parseInt(args[1]));
+    }
 }
 */
+
